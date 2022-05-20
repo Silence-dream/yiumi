5c431e7f282d0ab13ececd24fa3085376ac7b719
silence_dream <1428482231@qq.com>
2022-05-20 15:38:40 +0800

    1.0.2

--- packages/yiumi/bin/yiumi.js
+++ packages/yiumi/bin/yiumi.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log('Hello Malia!')
+const {
+  program
+} = require('commander');
+
+program
+  .version(require('../package.json').version, '-v, -V', '输出当前框架的版本')
+  .description('这是21天短文，挑战手写前端框架的产物框架')
+  .usage('<command> [options]')
+  .parse(process.argv)
+
+program.command("help")
+.alias("-h")
+.description("帮助命令")
+.action(function(name,other){
+  console.log(`
+  框架支持的命令
+  version,-v,-V 输出当前版本
+  help,-h 输出帮助
+  `);
+})
+.parse(process.argv)
